d0965ccf34c9d38ed5d468a93cb3917ce8521a50
patryk025 <43010113+patryk025@users.noreply.github.com>
2023-01-21 09:21:17 +0100

    Naprawiono widok chatu

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Hosting;
 use App\Models\TicketStatus;
+use App\Models\TicketMessage;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- resources/views/livewire/ticket/ticket-chat.blade.php
+++ resources/views/livewire/ticket/ticket-chat.blade.php
@@ -1,18 +1,91 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('translations.menu.ticket') }}
+      {{ $ticket->title }} (numer zgłoszenia: {{$ticket->id}})
     </h2>
   </x-slot>
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-        <?php dd($ticket->ticket_messages()); ?>
-        @foreach ($ticket->ticket_messages() as $message)
-          <span>Od $message->message_from : $message->message</span>
-        @endforeach
+        <style>
+          .bubbleWrapper {
+            padding: 10px 10px;
+            display: flex;
+            justify-content: flex-end;
+            flex-direction: column;
+            align-self: flex-end;
+            color: #fff;
+          }
+
+          .inlineContainer {
+            display: inline-flex;
+          }
+
+          .inlineContainer.own {
+            flex-direction: row-reverse;
+          }
+
+          .ownBubble {
+            min-width: 60px;
+            max-width: 700px;
+            padding: 14px 18px;
+            margin: 6px 8px;
+            background-color: #5b5377;
+            border-radius: 16px 16px 0 16px;
+            border: 1px solid #443f56;
+          }
+
+          .otherBubble {
+            min-width: 60px;
+            max-width: 700px;
+            padding: 14px 18px;
+            margin: 6px 8px;
+            background-color: #6C8EA4;
+            border-radius: 16px 16px 16px 0;
+            border: 1px solid #54788e;
+          }
+
+          .own {
+            align-self: flex-end;
+          }
+
+          .other {
+            align-self: flex-start;
+          }
+
+          span.own, span.other {
+            font-size: 14px;
+            color: grey;
+          }
+
+          .ticketChat {
+            width: 100%;
+          }
+        </style>
+        <div class="ticketChat">
+          <div class="ticketChatMessages">
+            @foreach ($ticket->ticket_messages()->get() as $message)
+              @if($message->message_from == 0)
+                <div class="bubbleWrapper">
+                  <div class="inlineContainer">
+                    <div class="otherBubble other">{{ $message->message }}</div>
+                  </div>
+                </div>
+              @else
+                <div class="bubbleWrapper">
+                  <div class="inlineContainer own">
+                    <div class="ownBubble own">{{ $message->message }}</div>
+                  </div>
+                </div>
+              @endif
+            @endforeach
+          </div>
+          <div class="ticketChatInput">
+            <input type="text" id="ticketChatInput">
+            <button id="ticketChatSend" onclick="alert('Not implemented')">Wyślij</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-
 </x-app-layout>
